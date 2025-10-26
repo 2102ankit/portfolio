bd5315f96d722ad2720e1309d4c20f5c8d2e6030
2102ankit <105378102+2102ankit@users.noreply.github.com>
2025-10-26 21:08:56 +0530

    overflow fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -97,7 +97,7 @@ export default function ProjectsPage() {
   }
 
   return (
-    <div className="py-12 px-4 sm:px-6 lg:px-8">
+    <div className="py-12 px-4 sm:px-6 lg:px-8 overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
